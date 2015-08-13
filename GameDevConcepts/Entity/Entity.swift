9b1a92740d6f21dd6359323c4c9eeef243d19f8c
Gabriel Anderson <Gabriel@Gabriels-MacBook-Pro.local>
2015-08-12 20:33:44 -0700

    Cleanup.

--- GameDevConcepts/Entity/Entity.swift
+++ GameDevConcepts/Entity/Entity.swift
@@ -36,14 +36,11 @@ public class Entity {
     var boundingCircle: Circle
     var time: CFAbsoluteTime = CFAbsoluteTimeGetCurrent()
     var currentAngle: Float = 0.0
-    var rotationAngle: Float = 0.0
-    var incAngle: Float = 0.0
     
     var animationX: Float = 0.0
     var animationY: Float = 0.0
     
     var animationState = Animation.RotateSetup
-    var hadAction: Bool = false
     
     let ACTION_DURATION = 0.9
     let ROTATE = 0.8
@@ -67,18 +64,15 @@ public class Entity {
             animationX = x
             animationY = y
             
-            incAngle = (atan2(y,x) - currentAngle) / (60 * Float(ROTATE))
-            
+            //incAngle = (atan2(y,x) - currentAngle) / (60 * Float(ROTATE))
+            rotate(animationX, y: animationY)
             animationState = Animation.Rotating
             time = CFAbsoluteTimeGetCurrent()
-            rotating(animationX,y: animationY)
-        case Animation.Rotating:
-            //rotating()
             
+        case Animation.Rotating:
             if CFAbsoluteTimeGetCurrent() - time >= ROTATE {
                 animationState = Animation.MoveSetup
             }
-           // NSLog("\nnow: \(now), time: \(time)")
             
         case Animation.MoveSetup:
             move(scene, x: animationX, y: animationY)
@@ -97,15 +91,14 @@ public class Entity {
         NSLog(animationState.toString())
     }
     
-    func rotating(x: Float, y: Float) {
+    func rotate(x: Float, y: Float) {
         let rotateAngle = atan2(y,x)
         let action = SKAction.rotateByAngle(CGFloat(rotateAngle-currentAngle), duration:0.9)
         body.shapeNode.runAction(action)
-        //body.buildShape(currentAngle)
+       
         currentAngle = rotateAngle
         
-       // NSLog("rotationAngle: \(180 * rotationAngle / Float(M_PI))")
-       // NSLog("incAngle: \(180 * incAngle / Float(M_PI)), currentAngle: \(180 * currentAngle / Float(M_PI))")
+         //body.buildShape(currentAngle)
         
     }
     
@@ -128,7 +121,6 @@ public class Entity {
             let action = SKAction.moveTo(CGPoint(x: CGFloat(moveX),y: CGFloat(moveY)), duration: ACTION_DURATION)
             body.shapeNode.runAction(action)
             boundingCircle.shapeNode.runAction(action)
-            hadAction = true
         }
         
         //NSLog("Body: \(body.x), \(body.y) || \(scene.size.width), \(scene.size.height)")
